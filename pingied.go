b65529fb06d07fe45a1f84c6106e9c4d54d19e72
Marco don Kaulea <DonMarco42@gmail.com>
2014-05-04 18:34:19 +0200

    Define RestAPI

--- pingied.go
+++ pingied.go
@@ -3,6 +3,7 @@ package main
 import "github.com/go-martini/martini"
 import "github.com/martini-contrib/binding"
 import "fmt"
+import "net/http"
 
 // DisplayMessage is a struct for formdata from /create/text calls
 type DisplayMessage struct {
@@ -10,11 +11,38 @@ type DisplayMessage struct {
 	Text string `form:"text" binding:"required"`
 }
 
-func createText(msg DisplayMessage) string {
-    return fmt.Sprintf("Hello %s\n", msg.Text)
+// ImageMessage is a struct to contain a file from /create/image calls
+type ImageMessage struct {
+}
+
+// IDMessage is a struct to contain a ID from /show/image and /show/scroll calls
+type IDMessage struct {
+    ID string `form:"id" binding:"required"`
+}
+
+func createText(msg DisplayMessage) (int, string) {
+    return http.StatusOK, fmt.Sprintf("Hello %s\n", msg.Text)
+}
+
+func createImage(_ ImageMessage) (int, string) {
+    var sha = ""
+    return http.StatusNotImplemented, sha
+}
+
+func showImage(_ IDMessage) (int, string) {
+    var sha = ""
+    return http.StatusNotImplemented, sha
+}
+
+func showScroll(_ IDMessage) (int, string) {
+    return http.StatusNotImplemented, ""
 }
 
 func main() {
 	m := martini.Classic()
 	m.Post("/create/text", binding.Bind(DisplayMessage{}), createText)
-	m.Run()}
+	m.Post("/create/image", binding.Bind(ImageMessage{}), createImage)
+	m.Post("/show/image", binding.Bind(IDMessage{}), showImage)
+	m.Post("/show/scroll", binding.Bind(IDMessage{}), showScroll)
+	m.Run()
+}
