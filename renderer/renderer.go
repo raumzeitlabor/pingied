f3721fd83a30ea82f6facecfab80af3e4a8dc1e4
Marco don Kaulea <DonMarco42@gmail.com>
2014-05-06 21:43:07 +0200

    Define renderer package and first function

---
+++ renderer/renderer.go
@@ -0,0 +1,11 @@
+// Package renderer provides functions to render images
+// from text in special fonts for 1-bit displays
+package renderer
+
+import "os"
+import "errors"
+
+// Render image from text using font
+func RenderImage(text string, font string) (file *os.File, err error) {
+    return nil, errors.New("Not implemented")
+}
