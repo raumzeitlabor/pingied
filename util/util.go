0d6d4b71518c0ad0827307dbdca903ebe87d592f
Marco don Kaulea <DonMarco42@gmail.com>
2014-05-10 00:22:01 +0200

    Add function declaration of RetrieveImage

--- util/util.go
+++ util/util.go
@@ -6,3 +6,7 @@ import "errors"
 func StoreImage(image []byte) (sha string, overridden bool, err error) {
 	return "", false, errors.New("Not implemented")
 }
+
+func RetrieveImage(sha string) (image []byte, err error) {
+    return []byte{0}, errors.New("Not implemented")
+}
